4e6638a39784f87ebd9e48b08a9e1352e434af2c
Kevin Cui <krazycavin@gmail.com>
2018-03-18 20:59:30 +0100

    Seperate actions

--- cook-raw.sh
+++ cook-raw.sh
@@ -3,13 +3,28 @@
 source cookbook.sh
 
 if [[ "$1" == "cook" || "$1" == "" ]];then
+    clean
 	fry
 	wrap
 	mix
+	check
+    clean
 fi
 
-if [[ "$1" == "check" || "$1" == "" ]];then
-	check
+if [[ "$1" == "fry" ]]; then
+    fry
+fi
+
+if [[ "$1" == "wrap" ]]; then
+    wrap
+fi
+
+if [[ "$1" == "mix" ]]; then
+    mix
+fi
+
+if [[ "$1" == "check" ]]; then
+    check
 fi
 
 if [[ "$1" == "clean" ]]; then
