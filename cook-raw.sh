2564b386c7e522352d24829603ab81be1ed95e0d
Kevin Cui <krazycavin@gmail.com>
2019-07-06 19:39:04 +0200

    Let script fail when cmd failed or var undeclared
    Why:
    More restrict and fail-safe
    
    See: [Best Practices for Writing Bash Scripts](http://kvz.io/blog/2013/11/21/bash-best-practices/)
    
    How:
    set -e/set -o errexit: script exit when a command fails
    set -u/set -o nounset: script exit when undeclared variable is used
    
    `set -u` causes bats test running failed, becasue BATS_TEST_SKIPPED is unbound variable.
    Ignore BATS_TEST_SKIPPED in bahn.bats

--- cook-raw.sh
+++ cook-raw.sh
@@ -4,6 +4,9 @@
 #/   ./cook-raw.sh [cook|cooked|cookfile|fry|wrap|mix|check|clean|unwrap] [file]
 #/
 
+set -e
+set -u
+
 set_var() {
     # Define variables/ingredients
     _RAW_EXTENSION=".CR2"
@@ -28,14 +31,13 @@ usage() {
 isCommandExist() {
     # Check if command exist
     if [ ! "$(command -v "$1")" ]; then
-        echo "$1 command doesn't exist!"
-        exit 1
+        echo "$1 command doesn't exist!" && exit 1
     fi
 }
 
 setExtension() {
     # Return file extension
-    if [ "$1" == "" ]; then
+    if [ "${1:-}" == "" ]; then
         echo $_RAW_EXTENSION
     else
         echo ${_RAW_EXTENSION}${_RECIPE_EXTENSION}
@@ -44,18 +46,22 @@ setExtension() {
 
 findFile() {
     # Find file has certain extension and file name
-    find ./*"$1" -maxdepth 1 -type f | grep "$2"
+    if [[ "${2:-}" == "" ]]; then
+        find ./*"$1" -maxdepth 1 -type f
+    else
+        find ./*"$1" -maxdepth 1 -type f | grep "$2"
+    fi
 }
 
 fry() {
     # Generate jpg file
-    echo "::FRY::"
+    echo "::FRY::" >&2
 	isCommandExist "dcraw"
 	isCommandExist "cjpeg"
 
 	mkdir -p $_JPG_OUTPUT
 
-    extension=$(setExtension "$1")
+    extension=$(setExtension "${1:-}")
     total=$(findFile "$extension" "$_FILE_SELECTED" | wc -w)
 	n=1
 	for i in $(findFile "$extension" "$_FILE_SELECTED"); do
@@ -63,10 +69,10 @@ fry() {
         raw=${basename}${_RAW_EXTENSION}
         jpg=${_JPG_OUTPUT}/${basename}${_JPG_EXTENSION}
 
-		echo "$n/$total Progressing file $raw..."
+		echo "$n/$total Progressing file $raw..." >&2
 
 		# convert raw to jpg
-		dcraw -c -w "$raw" | cjpeg -quality $_JPG_QUALITY -optimize -progressive > "$jpg";
+		dcraw -c -w "$raw" | cjpeg -quality $_JPG_QUALITY -optimize -progressive > "$jpg" || true
 
 		n=$((n+1))
 	done
@@ -74,12 +80,12 @@ fry() {
 
 wrap() {
     # Create zip file
-    echo "::WRAP::"
+    echo "::WRAP::" >&2
 	isCommandExist "zip"
 
 	mkdir -p $_ZIP_OUTPUT
 
-    extension=$(setExtension "$1")
+    extension=$(setExtension "${1:-}")
     total=$(findFile "$extension" "$_FILE_SELECTED" | wc -w)
     n=1
 	for i in $(findFile "$extension" "$_FILE_SELECTED"); do
@@ -89,14 +95,14 @@ wrap() {
         jpg=${_JPG_OUTPUT}/${basename}${_JPG_EXTENSION}
         zip=${_ZIP_OUTPUT}/${basename}.zip
 
-        echo "$n/$total Zipping file $raw..."
+        echo "$n/$total Zipping file $raw..." >&2
 
-        if [[ "$1" == "" ]]; then
+        if [[ "${1:-}" == "" ]]; then
             zip -j "$zip" "$raw" "$jpg"
-        elif [[ "$1" != "" && -f "$recipe" ]]; then
+        elif [[ "${1:-}" != "" && -f "$recipe" ]]; then
             zip -j "$zip" "$raw" "$jpg" "$recipe"
         else
-            echo "Skip $raw"
+            echo "Skip $raw" >&2
         fi
 
         n=$((n+1))
@@ -105,10 +111,10 @@ wrap() {
 
 mix() {
     # Mix zip and jpg
-    echo "::MIX::"
+    echo "::MIX::" >&2
 	mkdir -p $_FINAL_OUTPUT
 
-    extension=$(setExtension "$1")
+    extension=$(setExtension "${1:-}")
     total=$(findFile "$extension" "$_FILE_SELECTED" | wc -w)
     n=1
 	for i in $(findFile "$extension" "$_FILE_SELECTED"); do
@@ -116,9 +122,9 @@ mix() {
         raw=${basename}${_RAW_EXTENSION}
         jpg=${_JPG_OUTPUT}/${basename}${_JPG_EXTENSION}
         zip=${_ZIP_OUTPUT}/${basename}.zip
-        final=${_FINAL_OUTPUT}/${basename}-editable${1}${_JPG_EXTENSION}
+        final=${_FINAL_OUTPUT}/${basename}-editable${1:-}${_JPG_EXTENSION}
 
-        echo "$n/$total Mixing file $raw..."
+        echo "$n/$total Mixing file $raw..." >&2
 
         # create final jpg
         cat "$jpg" "$zip" > "$final"
@@ -129,7 +135,7 @@ mix() {
 
 check() {
     # Check cooked file
-    echo "::CHECK::"
+    echo "::CHECK::" >&2
     isCommandExist "unzip"
     isCommandExist "md5sum"
 
@@ -138,25 +144,25 @@ check() {
     md5sum=${_CHECK_OUTPUT}/${_MD5SUM_FILE}
     true > $md5sum
 
-    extension=$(setExtension "$1")
+    extension=$(setExtension "${1:-}")
     total=$(findFile "$extension" "$_FILE_SELECTED" | wc -w)
     n=1
 	for i in $(findFile "$extension" "$_FILE_SELECTED"); do
         basename=$(basename "$i" "$extension")
         raw=${basename}${_RAW_EXTENSION}
         recipe=${basename}${_RAW_EXTENSION}${_RECIPE_EXTENSION}
-        final=${_FINAL_OUTPUT}/${basename}-editable${1}${_JPG_EXTENSION}
+        final=${_FINAL_OUTPUT}/${basename}-editable${1:-}${_JPG_EXTENSION}
 
-        echo "$n/$total Preparing file $raw..."
+        echo "$n/$total Preparing file $raw..." >&2
 
         # create md5sum file
         md5sum "$raw" >> $md5sum
-        if [ "$1" != "" ]; then
+        if [ "${1:-}" != "" ]; then
             md5sum "$recipe" >> $md5sum
         fi
 
         # unzip raw files
-        unzip "$final" -d ${_CHECK_OUTPUT} 2> /dev/null
+        unzip "$final" -d ${_CHECK_OUTPUT} 2> /dev/null || true
 
         n=$((n+1))
     done
@@ -164,14 +170,14 @@ check() {
     # checsum comparision
     cd "$_CHECK_OUTPUT" || return
 
-    echo -e "\\nChecking files in $(pwd)..."
+    echo -e "\\nChecking files in $(pwd)..." >&2
     md5sum -c ${_MD5SUM_FILE}
     cd ..
 }
 
 unwrap() {
     # Extract raw files
-    echo "::UNWRAP::"
+    echo "::UNWRAP::" >&2
 	isCommandExist "unzip"
 
 	mkdir -p $_RAW_OUTPUT
@@ -179,7 +185,7 @@ unwrap() {
     total=$(findFile "$_JPG_EXTENSION" "" | wc -w)
     n=1
 	for i in $(findFile "$_JPG_EXTENSION" ""); do
-		echo "$n/$total Extract file $i..."
+		echo "$n/$total Extract file $i..." >&2
 		unzip -o "$i" -d $_RAW_OUTPUT 2> /dev/null
 		n=$((n+1))
 	done
@@ -189,7 +195,7 @@ unwrap() {
 
 clean() {
     # Remove temporary folders
-    rm -r ${_CHECK_OUTPUT} ${_ZIP_OUTPUT} ${_JPG_OUTPUT} 2> /dev/null
+    rm -r ${_CHECK_OUTPUT} ${_ZIP_OUTPUT} ${_JPG_OUTPUT} 2> /dev/null || true
 }
 
 cook() {
@@ -214,8 +220,7 @@ cookRecipe() {
 
 cookOneFile() {
     if [ ! -f "$1" ]; then
-        echo "$1 doesn't exist."
-        exit 1
+        echo "$1 doesn't exist." && exit 1
     fi
 
     _FILE_SELECTED="$1"
@@ -226,8 +231,7 @@ cookOneFile() {
     elif [ "$extension" == "$_RECIPE_EXTENSION" ]; then
         cookRecipe
     else
-        echo "Unsupport file type $extension"
-        exit 1
+        echo "Unsupport file type $extension" && exit 1
     fi
 
     _FILE_SELECTED=""
@@ -236,34 +240,39 @@ cookOneFile() {
 main() {
     set_var
 
+    local cmd
+    local file
+    cmd=${1:-}
+    file=${2:-}
+
     expr "$*" : ".*--help" > /dev/null && usage
 
-    #/     cook:            cook all raw files
-    [[ "$1" == "cook" || "$1" == "" ]] && cook
+#/     cook:            cook all raw files
+    [[ "$cmd" == "cook" || "$cmd" == "" ]] && cook
 
-    #/     cooked:          add "cooked" tag in all final files
-    [[ "$1" == "cooked" ]] && cookRecipe
+#/     cooked:          add "cooked" tag in all final files
+    [[ "$cmd" == "cooked" ]] && cookRecipe
 
-    #/     cookfile <file>: cook a specific file
-    [[ "$1" == "cookfile" ]] && cookOneFile "$2"
+#/     cookfile <file>: cook a specific file
+    [[ "$cmd" == "cookfile" ]] && cookOneFile "$file"
 
-    #/     fry <file>:      convert raw file to jpg
-    [[ "$1" == "fry" ]] && fry "$2"
+#/     fry <file>:      convert raw file to jpg
+    [[ "$cmd" == "fry" ]] && fry "$file"
 
-    #/     wrap <file>:     create zip file
-    [[ "$1" == "wrap" ]] && wrap "$2"
+#/     wrap <file>:     create zip file
+    [[ "$cmd" == "wrap" ]] && wrap "$file"
 
-    #/     mix <file>:      mix zip file and jpg file
-    [[ "$1" == "mix" ]] && mix "$2"
+#/     mix <file>:      mix zip file and jpg file
+    [[ "$cmd" == "mix" ]] && mix "$file"
 
-    #/     check <file>:    run a check
-    [[ "$1" == "check" ]] && check "$2"
+#/     check <file>:    run a check
+    [[ "$cmd" == "check" ]] && check "$file"
 
-    #/     clean:           remove temporary folders
-    [[ "$1" == "clean" ]] && clean
+#/     clean:           remove temporary folders
+    [[ "$cmd" == "clean" ]] && clean
 
-    #/     unwrap:          extract raw files
-    [[ "$1" == "unwrap" ]] && unwrap
+#/     unwrap:          extract raw files
+    [[ "$cmd" == "unwrap" ]] && unwrap
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
--- test/cook-raw.bats
+++ test/cook-raw.bats
@@ -4,20 +4,37 @@
 #   ~$ bats test/cow-raw.bats
 #
 
+BATS_TEST_SKIPPED=
+
 setup() {
     _SCRIPT="cook-raw.sh"
-    _TEST_OUTPUT="$(pwd)/test/"
-    source "$_SCRIPT"
-    set_var
+    _HOME="$(pwd)"
+    _TEST_OUTPUT="$_HOME/test"
+    _RAW_EXTENSION=".CR2"
+    _RECIPE_EXTENSION=".xmp"
+    _JPG_EXTENSION=".jpg"
+    _JPG_QUALITY=85
+    _JPG_OUTPUT="$_TEST_OUTPUT/jpg"
+    _ZIP_OUTPUT="$_TEST_OUTPUT/zip"
+    _FINAL_OUTPUT="$_TEST_OUTPUT/final"
+    _CHECK_OUTPUT="$_TEST_OUTPUT/check"
+    _MD5SUM_FILE="md5sum"
+    _FILE_SELECTED=""
+
+    teardown
+
     echo "001.raw" > "$_TEST_OUTPUT/IMG_001${_RAW_EXTENSION}"
     echo "002.raw" > "$_TEST_OUTPUT/IMG_002${_RAW_EXTENSION}"
     echo "002.raw.cooked" > "$_TEST_OUTPUT/IMG_002${_RAW_EXTENSION}${_RECIPE_EXTENSION}"
+
+    source "$_SCRIPT"
 }
 
 teardown() {
     cd "$_TEST_OUTPUT"
     rm -f IMG_001* IMG_002*
     rm -rf "$_CHECK_OUTPUT" "$_ZIP_OUTPUT" "$_JPG_OUTPUT" "$_FINAL_OUTPUT" 2>/dev/null
+    cd "$_HOME"
 }
 
 checkFileExist() {
@@ -54,6 +71,7 @@ checkmd5sum() {
 @test "CHECK: setExtension() default extension" {
     _RAW_EXTENSION="rawextension"
     run setExtension
+    [ "$status" -eq 0 ]
     [ "$output" = "$_RAW_EXTENSION" ]
 }
 
@@ -61,6 +79,7 @@ checkmd5sum() {
     _RAW_EXTENSION="rawextension"
     _RECIPE_EXTENSION="recipe"
     run setExtension true
+    [ "$status" -eq 0 ]
     [ "$output" = "${_RAW_EXTENSION}${_RECIPE_EXTENSION}" ]
 }
 
@@ -74,10 +93,11 @@ checkmd5sum() {
 @test "CHECK: findFile() no matches" {
     cd $_TEST_OUTPUT
     run findFile "CR2" "filedoesnotexist"
+    [ "$status" -eq 1 ]
     [ "$output" = "" ]
 }
 
-@test "CHECK: clearn()" {
+@test "CHECK: clean()" {
     mkdir -p "$_CHECK_OUTPUT"
     mkdir -p "$_ZIP_OUTPUT"
     mkdir -p "$_JPG_OUTPUT"
@@ -85,12 +105,16 @@ checkmd5sum() {
     [ "$(checkFolderExist $_ZIP_OUTPUT)" = "PASS" ]
     [ "$(checkFolderExist $_JPG_OUTPUT)" = "PASS" ]
     run clean
+    [ "$status" -eq 0 ]
     [ "$(checkFolderNotExist $_CHECK_OUTPUT)" = "PASS" ]
     [ "$(checkFolderNotExist $_ZIP_OUTPUT)" = "PASS" ]
     [ "$(checkFolderNotExist $_JPG_OUTPUT)" = "PASS" ]
 }
 
 @test "CHECK: cook-raw.sh clean" {
+    set_var() {
+        echo "" > /dev/null
+    }
     mkdir -p "$_CHECK_OUTPUT"
     mkdir -p "$_ZIP_OUTPUT"
     mkdir -p "$_JPG_OUTPUT"
@@ -106,6 +130,7 @@ checkmd5sum() {
 @test "CHECK: fry()" {
     cd "$_TEST_OUTPUT"
     run fry
+    [ "$status" -eq 0 ]
     [ "$(checkFileExist ${_JPG_OUTPUT}/IMG_001${_JPG_EXTENSION})" = "PASS" ]
     [ "$(checkFileExist ${_JPG_OUTPUT}/IMG_002${_JPG_EXTENSION})" = "PASS" ]
 }
@@ -121,6 +146,7 @@ checkmd5sum() {
     cd "$_TEST_OUTPUT"
     run main fry
     run wrap
+    [ "$status" -eq 0 ]
     [ "$(checkFileExist ${_ZIP_OUTPUT}/IMG_001.zip)" = "PASS" ]
     [ "$(checkFileExist ${_ZIP_OUTPUT}/IMG_002.zip)" = "PASS" ]
 }
@@ -138,6 +164,7 @@ checkmd5sum() {
     run main fry
     run main wrap
     run mix
+    [ "$status" -eq 0 ]
     [ "$(checkFileExist ${_FINAL_OUTPUT}/IMG_001-editable${_JPG_EXTENSION})" = "PASS" ]
     [ "$(checkFileExist ${_FINAL_OUTPUT}/IMG_002-editable${_JPG_EXTENSION})" = "PASS" ]
 }
@@ -159,6 +186,7 @@ checkmd5sum() {
     run main wrap
     run main mix
     run check
+    [ "$status" -eq 0 ]
     [ "$(checkFileExist ${_CHECK_OUTPUT}/IMG_001${_RAW_EXTENSION})" = "PASS" ]
     [ "$(checkFileNotExist ${_CHECK_OUTPUT}/IMG_001${_RAW_EXTENSION}${_RECIPE_EXTENSION})" = "PASS" ]
     [ "$(checkFileExist ${_CHECK_OUTPUT}/IMG_002${_RAW_EXTENSION})" = "PASS" ]
@@ -188,6 +216,7 @@ checkmd5sum() {
 }
 
 @test "CHECK: cook-raw.sh unwrap" {
+    _RAW_OUTPUT="./raw"
     cd "$_TEST_OUTPUT"
     run main fry "recipe"
     run main wrap "recipe"
@@ -195,6 +224,7 @@ checkmd5sum() {
     run main check "-cooked"
     cd "$_FINAL_OUTPUT"
     run main unwrap
+    [ "$status" -eq 0 ]
     [ "$(checkFileExist ${_RAW_OUTPUT}/IMG_002${_RAW_EXTENSION})" = "PASS" ]
     [ "$(checkFileExist ${_RAW_OUTPUT}/IMG_002${_RAW_EXTENSION}${_RECIPE_EXTENSION})" = "PASS" ]
     [ "$(checkFileNotExist ${_RAW_OUTPUT}/IMG_002${_JPG_EXTENSION})" = "PASS" ]
@@ -205,6 +235,7 @@ checkmd5sum() {
 @test "CHECK: cookOneFile() raw file" {
     cd "$_TEST_OUTPUT"
     run cookOneFile "IMG_001${_RAW_EXTENSION}"
+    [ "$status" -eq 0 ]
     [ "$(checkFolderNotExist ${_JPG_OUTPUT})" = "PASS" ]
     [ "$(checkFolderNotExist ${_ZIP_OUTPUT})" = "PASS" ]
     [ "$(checkFolderNotExist ${_CHECK_OUTPUT})" = "PASS" ]
@@ -217,6 +248,7 @@ checkmd5sum() {
 @test "CHECK: cookOneFile() recipe file" {
     cd "$_TEST_OUTPUT"
     run cookOneFile "IMG_002${_RAW_EXTENSION}${_RECIPE_EXTENSION}"
+    [ "$status" -eq 0 ]
     [ "$(checkFolderNotExist ${_JPG_OUTPUT})" = "PASS" ]
     [ "$(checkFolderNotExist ${_ZIP_OUTPUT})" = "PASS" ]
     [ "$(checkFolderNotExist ${_CHECK_OUTPUT})" = "PASS" ]
@@ -229,6 +261,7 @@ checkmd5sum() {
 @test "CHECK: cook()" {
     cd "$_TEST_OUTPUT"
     run cook
+    [ "$status" -eq 0 ]
     [ "$(checkFolderNotExist ${_JPG_OUTPUT})" = "PASS" ]
     [ "$(checkFolderNotExist ${_ZIP_OUTPUT})" = "PASS" ]
     [ "$(checkFolderNotExist ${_CHECK_OUTPUT})" = "PASS" ]
@@ -241,6 +274,7 @@ checkmd5sum() {
 @test "CHECK: cookRecipe()" {
     cd "$_TEST_OUTPUT"
     run cookRecipe
+    [ "$status" -eq 0 ]
     [ "$(checkFolderNotExist ${_JPG_OUTPUT})" = "PASS" ]
     [ "$(checkFolderNotExist ${_ZIP_OUTPUT})" = "PASS" ]
     [ "$(checkFolderNotExist ${_CHECK_OUTPUT})" = "PASS" ]
